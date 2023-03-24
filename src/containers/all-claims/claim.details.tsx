5b7a407b24c755aa02eb576cdd5f7bcaba020dbf
ajmaljalal <jalalajmal96@gmail.com>
2023-03-23 21:22:39 -0700

    claim details wip

--- src/containers/all-claims/claim.details.tsx
+++ src/containers/all-claims/claim.details.tsx
@@ -6,8 +6,11 @@ import { icons } from '@/lib/icons'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { data } from './data'
 
 export const ClaimDetailsContainer = () => {
+  const claim = data.filteredClaims[0]
+
   return (
     <MainContainer>
       <Container>
@@ -16,7 +19,7 @@ export const ClaimDetailsContainer = () => {
             <Link href={`/all-claims`}>
               <h3 className='flex items-center gap-4'>
                 <Image src={icons.arrowLeft} alt='arrow-left' />
-                Claim Details
+                {claim.vendor}
               </h3>
             </Link>
             <div className='flex'>
@@ -30,6 +33,26 @@ export const ClaimDetailsContainer = () => {
           </div>
         </div>
         <Divider />
+        <p>
+          {claim.program}
+          <span className='px-2 py-[5px] bg-pDark text-pWhite rounded-[24px] small-caption ml-10'>
+            {claim.frequency}
+          </span>
+        </p>
+        <p>Requested: <span>{claim.requested}</span></p>
+        <p>Reimbursed: <span>{claim.reimbursed}</span></p>
+        <p>Status: <span>{claim.status}</span></p>
+        <p>Submitted on: <span>{claim.date}</span></p>
+        <p>Approved on: <span>{claim.date}</span></p>
+        <div>
+          <p>Attachment</p>
+        </div>
+        <div>
+          <p>Description</p>
+        </div>
+        <div>
+          <p>Comments</p>
+        </div>
       </Container>
     </MainContainer>
   )
