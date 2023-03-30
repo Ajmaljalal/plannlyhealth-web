a224ffc7c2db26ec07539676a0359b6aaafdab4e
ajmaljalal <jalalajmal96@gmail.com>
2023-03-30 12:47:02 -0700

    back-office ui

---
+++ src/containers/back-office/data.ts
@@ -0,0 +1,29 @@
+const users = [
+  {
+    id: 1,
+    name: 'Kausar Jalal',
+    email: 'ajmaljalal@test.com',
+    status: 'Active',
+    role: 'Wellness Coordinator',
+  },
+  {
+    id: 2,
+    name: 'Ajmal Jalal',
+    email: 'ajmaljalal@test.com',
+    status: 'Deactivated',
+    role: 'Customer Support',
+  },
+  {
+    id: 3,
+    name: 'Meena Jalal',
+    email: 'ajmaljalal@test.com',
+    status: 'Active',
+    role: 'Wellness Coordinator',
+  }
+]
+
+let filteredUsers = users
+
+export const data = {
+  filteredUsers,
+}
---
+++ src/containers/back-office/three-dots-button.tsx
@@ -0,0 +1,109 @@
+'use client'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { DropdownMenu } from '@/components/dropdown-menu'
+import { icons } from '@/lib/icons'
+import { ConfirmModal } from './confirm-modal'
+import { UserAddEditModal } from './add-edit-modal'
+
+
+const styles = `flex items-center rounded-[4px] hover:bg-pLight w-[24px] h-[24px] py-[4px] cursor-pointer`
+
+interface ThreeDotsButtonProps {
+  user: any
+}
+
+export const ThreeDotsButton = ({ user }: ThreeDotsButtonProps) => {
+  const [editModalOpen, setEditModalOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
+  const [confirmationModalOpen, setConfirmationModalOpen] = useState(false)
+  const [title, setTitle] = useState('null')
+
+
+  const toggleDropdown = (isNowOpen: boolean) => {
+    if (isOpen) {
+      setIsOpen(isNowOpen)
+      return
+    }
+    setIsOpen(isNowOpen)
+  }
+
+  const toggleConfirmationModal = (text: string) => {
+    setConfirmationModalOpen(!confirmationModalOpen)
+    setTitle(text)
+  }
+
+  const toggleEditModal = () => {
+    setEditModalOpen(!editModalOpen)
+  }
+
+  const handleActivate = () => {
+    console.log('Approve', user)
+    // TODO: Add approve user functionality
+    toggleConfirmationModal('null')
+  }
+
+  const handleDeactivate = () => {
+    console.log('Decline', user)
+    // TODO: Add decline user functionality
+    toggleConfirmationModal('null')
+  }
+
+  const handleResendInvite = () => {
+    console.log('Delete', user)
+    // TODO: Add delete user functionality
+    toggleConfirmationModal('null')
+  }
+
+  const handleResendPasswordReset = () => {
+    console.log('Delete', user)
+    // TODO: Add delete user functionality
+    toggleConfirmationModal('null')
+  }
+
+  const confirmActions: any = {
+    'Send Password Reset': handleResendPasswordReset,
+    'Send Invite': handleResendInvite,
+    'Activate': handleActivate,
+    'Deactivate': handleDeactivate
+  }
+
+  return (
+    <div className={styles}>
+      <Image src={icons.threeDots} alt='three dots menu' onClick={() => toggleDropdown(isOpen ? false : true)} />
+      <DropdownMenu
+        isOpen={isOpen}
+        onClose={toggleDropdown}
+        width='w-[180px]'
+        menuItems={[
+          {
+            element: 'Edit',
+            onClick: toggleEditModal
+          },
+          {
+            element: 'Resend Invite',
+            onClick: () => toggleConfirmationModal('Send Invite')
+          },
+          {
+            element: 'Send Password Reset',
+            onClick: () => toggleConfirmationModal('Send Password Reset')
+          },
+          {
+            element: 'Deactivate',
+            onClick: () => toggleConfirmationModal('Deactivate')
+          },
+        ]}
+      />
+      {/* TODO: render modals here */}
+      <ConfirmModal
+        isOpen={confirmationModalOpen}
+        onClose={toggleConfirmationModal}
+        title={title}
+        onConfirm={confirmActions[title]}
+        // TODO: add user name
+        userName={user.name}
+      />
+      <UserAddEditModal isOpen={editModalOpen} onClose={toggleEditModal} user={user} />
+    </div>
+  )
+}
