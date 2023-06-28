07fe5be436329a6528654e99d42b611f3bc4e863
ajmaljalal <jalalajmal96@gmail.com>
2023-06-27 23:29:13 -0400

    favicon and style updates

--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -19,7 +19,7 @@ const LoginContainer = () => {
   const renderSignInError = () => {
     if (session?.data?.user?.name) {
       return (
-        <div className="w-[360px] h-full flex flex-col items-center justify-center px-[24px]">
+        <div className="w-[360px] h-full flex flex-col items-center px-[24px] text-center">
           <img src="/logos/ph-logo-dark.svg" alt="Plannly" className="mr-[27px]" />
           <h2 className="text-[32px] text-simibold mb-[12px]">User Does Not Exist!</h2>
           <p className="text-darkgrey mb-[32px]">Please contact support for help!</p>
@@ -32,9 +32,9 @@ const LoginContainer = () => {
   const renderForm = () => {
     if (!session?.data?.user?.name) {
       return (
-        <div className="w-[440px] h-full flex flex-col items-center justify-center px-[8px]">
+        <div className="w-[440px] h-full flex flex-col items-center px-[8px] text-center">
           <img src="/logos/ph-logo-dark.svg" alt="Plannly" className="mr-[27px]" />
-          <h2 className="text-[32px] text-simibold mb-[12px]">Welcome to Plannly Health!</h2>
+          <h2 className="text-[32px] text-simibold mb-[12px] text-center">Welcome to Plannly Health!</h2>
           <p className="text-darkgrey mb-[32px]">Login to continue</p>
           <Button text="Continue with Google" className="w-full mb-[16px]" icon={icons.googleIcon} onClick={handleGoogleSignIn} />
           {/* <Button text="Continue with Microsoft" className="w-full" icon={icons.microsoftIcon} onClick={() => console.log('clicked')} /> */}
@@ -51,7 +51,7 @@ const LoginContainer = () => {
       w-full 
       h-full 
       justify-center 
-      custom-bg
+      xl:custom-bg
       ">
       {renderSignInError()}
       {renderForm()}
