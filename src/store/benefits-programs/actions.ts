706e27b03bdbed3748cc195f2ccc758e8d8af254
Ajmaljalal <jalalajmal96@gmail.com>
2023-07-10 20:33:27 -0500

    unused folders removed

--- src/store/benefits-programs/actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from "@reduxjs/toolkit"
-export const setPrograms = createAction<any>('programs/setPrograms')
-export const setCurrentProgram = createAction<any>('programs/setCurrentProgram')
-export const updatePrograms = createAction<any>('programs/updatePrograms')
