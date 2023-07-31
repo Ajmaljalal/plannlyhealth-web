2edd45d1403f7a890c9e9f3ae873af1139153729
ajmaljalal <jalalajmal96@gmail.com>
2023-07-31 12:20:08 -0700

    nav item resp update

--- src/components/left-nav/nav-item.tsx
+++ src/components/left-nav/nav-item.tsx
@@ -13,11 +13,13 @@ interface NavItemProps {
 
 const navItemStyles = `
     flex
+    gap-0
+    lg:gap-2
     items-center
-    justify-start
-    md:justify-center
+    justify-center
+    lg:justify-start
     w-[40px] 
-    md:w-full
+    lg:w-full
     h-[40px]
     text-[14px]
     font-bold-700
@@ -29,7 +31,6 @@ const navItemStyles = `
     hover:text-basic_white
     my-[9px]
     cursor-pointer
-    leading-[1.2]
     focus:bg-brand_voilet
     focus:text-basic_white
 `
@@ -42,7 +43,7 @@ export const NavItem = ({ text, href, icon, iconLight }: NavItemProps) => {
   return (
     <Link href={href} className={`${navItemStyles} ${isActive ? 'bg-brand_voilet text-basic_white' : ''}`}>
       {/* <div className="flex items-center sm:justify-start justify-center"> */}
-      <Image src={currentIcon} alt={`${text} icon`} width='18' height='18' className='mr-[10px]' />
+      <Image src={currentIcon} alt={`${text} icon`} width='18' height='18' className='' />
       <span className="sm:block hidden">{text}</span>
       {/* </div> */}
     </Link>
