5681ac656f089dc273eda668321785e53b4adcf1
ajmaljalal <jalalajmal96@gmail.com>
2023-08-10 17:47:00 -0700

    Revert "user logins wip"
    This reverts commit 6541fdf790511dacfdff834f33a01187a03054e4.

--- src/containers/auth/login/index.tsx
+++ src/containers/auth/login/index.tsx
@@ -1,52 +1,56 @@
 "use client"
-import { useEffect } from "react";
 import { Button } from "@/components/button";
-import { useMsal } from '@azure/msal-react';
 import { icons } from "@/lib/icons";
-import { loginRequest } from "@/lib/services/auth";
-import { useDispatch, useSelector } from "@/store/store";
-import { setUser, userProfileSelector } from "@/store/user";
-// import { useSession, signIn } from "next-auth/react"
+import { loginRequest, signInWithMicrosoft } from "@/lib/services/auth";
+import { useSession, signIn } from "next-auth/react"
+import { useMsal } from '@azure/msal-react';
+import { useEffect, useState } from "react";
 
-const LoginContainer = async () => {
-  // const session = useSession();
+const LoginContainer = () => {
+  const session = useSession();
   const { instance } = useMsal();
-  const activeAccount = instance.getActiveAccount() || undefined
-  // const token = await instance.acquireTokenSilent(
-  //   {
-  //     ...loginRequest,
-  //     account: activeAccount
-  //   }
-  // )
-  const user = useSelector(userProfileSelector);
-  const dispatch = useDispatch();
+  const [userName, setUserName] = useState("");
 
+  const activeAccount = instance.getActiveAccount();
   useEffect(() => {
     if (activeAccount) {
-      dispatch(setUser(activeAccount))
+      setUserName(activeAccount.username);
+    } else {
+      setUserName("");
     }
-  }, []);
+  }, [activeAccount]);
 
-  // const handleGoogleSignIn = async (event: any) => {
+  const handleGoogleSignIn = async (event: any) => {
+    try {
+      await signIn('google')
+    } catch (error: any) {
+    }
+  };
+
+  // const handleMicrosoftSignIn = async (event: any) => {
   //   try {
-  //     await signIn('google')
+  //     // const result = await signIn('azure-ad')
+  //     // const result = await signInWithMicrosoft()
+  //     await signIn('azure-ad')
+  //     // console.log('result', result)
+
   //   } catch (error: any) {
   //     console.log('error', error)
   //   }
   // };
 
-  const handleMicrosoftLogin = async () => {
+  const handleLogin = async () => {
     await instance.loginRedirect(loginRequest);
   }
 
+  console.log('session', session)
   const renderSignInError = () => {
-    if (user) {
+    if (session?.data?.user?.name || userName) {
       return (
         <div className="w-[360px] flex flex-col items-center px-[24px] text-center">
           <img src="/logos/logo-icon-only-v2.svg" alt="Plannly" className="mb-[32px]" width={70} height={70} />
           <h2 className="text-normal mb-[12px]">User Does Not Exist!</h2>
           <p className="text-basic_grey_1 mb-[32px]">Please contact support for help!</p>
-          <p>{user?.username}</p>
         </div>
       )
     } else {
@@ -55,14 +59,14 @@ const LoginContainer = async () => {
   }
 
   const renderForm = () => {
-    if (!user) {
+    if (!session?.data?.user?.name && !userName) {
       return (
         <div className="w-[440px] flex flex-col items-center px-[8px] text-center">
           <img src="/logos/logo-icon-only-v2.svg" alt="Plannly" className="mb-[32px]" width={50} height={50} />
           <h2 className="mb-[12px] text-center">Welcome Back!</h2>
           <p className="text-basic_grey_1 mb-[32px]">Login to Plannly Health to continue</p>
-          {/* <Button text="Continue with Google" className="mb-[16px] w-[300px]" icon={icons.googleIcon} onClick={handleGoogleSignIn} /> */}
-          <Button text="Continue with Microsoft" className="w-[300px]" icon={icons.microsoftIcon} onClick={handleMicrosoftLogin} />
+          <Button text="Continue with Google" className="mb-[16px] w-[300px]" icon={icons.googleIcon} onClick={handleGoogleSignIn} />
+          <Button text="Continue with Microsoft" className="w-[300px]" icon={icons.microsoftIcon} onClick={handleLogin} />
         </div>
       )
     } else {
