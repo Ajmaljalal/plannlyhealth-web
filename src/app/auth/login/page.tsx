0b3e6ebb3d50289eb81acbc5bee112ca4c5fdf18
ajmaljalal <jalalajmal96@gmail.com>
2024-10-06 16:49:27 -0700

    login updates

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import LoginContainer from '@/containers/auth/login'
+import LoginContainer from '@/containers/auth/login/index'
 
 import React from 'react'
 
--- src/containers/auth/login/index.tsx
+++ src/containers/auth/login/index.tsx
@@ -1,84 +1,102 @@
-// "use client"
+"use client"
 import { Button } from "@/components/button";
-import { useSession, signIn } from "next-auth/react";
-import { useEffect, useState, useCallback } from "react";
+import { icons } from "@/lib/icons";
+import { loginRequest } from "@/lib/services/auth";
+import { useSession, signIn } from "next-auth/react"
+import { useMsal } from '@azure/msal-react';
+import { useEffect, useState } from "react";
 import { useDispatch } from "@/store/store";
 import { setUser } from "@/store/user";
 import { useRouter } from "next/navigation";
-import { Input } from "@/components/input";
-import { getEmployeeByEmail } from "@/lib/services/employee";
+import axios from "axios";
+
+const employeeBaseURL = process.env.NEXT_PUBLIC_PLANNLY_API_URL
 
 const LoginContainer = () => {
-  const [isEmployeeExist, setIsEmployeeExist] = useState(false);
-  const [error, setError] = useState(false);
-  const [emailSent, setEmailSent] = useState(false);
+  const [noEmployeeAccount, setNoEmployeeAccount] = useState(false);
   const [email, setEmail] = useState('');
-  const { data: sessionData, status: sessionStatus } = useSession();
+  const session = useSession();
+  const { instance } = useMsal();
   const dispatch = useDispatch();
   const router = useRouter();
 
-  const handleSignIn = useCallback(async () => {
-    if (!email.trim()) return;
+  // const activeAccount = instance.getActiveAccount();
 
-    try {
-      const user = await signIn('email', {
-        email,
-        redirect: false,
-        callbackUrl: "/",
-      });
-
-      if (user?.error) {
-        setError(true);
-      } else {
-        setEmailSent(true);
-      }
-    } catch (err) {
-      console.error(err);  // Log errors to console for easier debugging
-    }
-  }, [email]);
-
-  const getEmployee = useCallback((user: any) => {  // Used useCallback for memoization
-    getEmployeeByEmail(user.email).then((employee) => {
+  const getEmployee = (user: any) => {
+    axios.get(`${employeeBaseURL}/employees/${user.sub}`).then((res) => {
+      const employee = res?.data
       if (employee) {
         dispatch(setUser({ ...user, ...employee }));
-        const redirectPath = {
-          'Super Admin': '/admin',
-          'Admin': '/company/dashboard',
-          'Standard': '/employee/rewards',
-        }[employee.role as string];
-
-        if (redirectPath) router.push(redirectPath);
+        if (employee.role === 'Super Admin') {
+          return router.push(`/admin`)
+        }
+        if (employee.role === 'Admin') {
+          return router.push(`/company/dashboard`)
+        }
+        if (employee.role === 'Standard') {
+          return router.push(`/employee/rewards`)
+        }
       } else {
-        setIsEmployeeExist(true);
+        setNoEmployeeAccount(true)
       }
-    }).catch(console.error);  // Log errors to console for easier debugging
-  }, [dispatch, router]);
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
 
   useEffect(() => {
-    const user = sessionData?.user;
-    if (user) getEmployee(user);
-  }, [sessionData, getEmployee]);
+    instance.acquireTokenSilent({
+      scopes: ['user.read'],
+      account: instance.getActiveAccount() as any
+    }).then((res) => {
+      console.log('res:', res)
+    }).catch((err) => {
+      console.log('err:', err)
 
-  const renderForm = useCallback(() => {  // Used useCallback for memoization
-    return sessionData?.user ? null : (
-      <div className="w-[440px] flex flex-col items-center px-[8px] text-center">
-        <div className="flex flex-col gap-2 items-center mb-[64px]">
-          <img src="/logos/logo-icon-only-v2.svg" alt="Plannly" className="" width={64} height={64} />
-          <h1 className="text-[40px] text-brand_voilet">Plannly Health</h1>
+    })
+  }, [instance]);
+
+  const handleMicrosoftLogin = async () => {
+    const result = await instance.loginRedirect(loginRequest);
+  }
+
+  console.log('result:', instance)
+
+
+  const renderForm = () => {
+    if (!session?.data?.user) {
+      return (
+        <div className="w-[440px] flex flex-col items-center px-[8px] text-center">
+          <div className="flex flex-col gap-2 items-center mb-[64px]">
+            <img src="/logos/logo-icon-only-v2.svg" alt="Plannly" className="" width={64} height={64} />
+            <h1 className="text-[40px] text-brand_voilet">Plannly Health</h1>
+          </div>
+          {/* <h2 className="mb-[12px] text-center">Welcome Back!</h2>
+          <p className="text-basic_grey_1 mb-[32px]">Login to Plannly Health to continue</p>
+          <Button text="Continue with Google" className="mb-[16px] w-[300px]" icon={icons.googleIcon} onClick={handleGoogleSignIn} />*/}
+          <Button text="Continue with Microsoft" className="w-[300px]" icon={icons.microsoftIcon} onClick={handleMicrosoftLogin} />
+          {/* <Input
+            type="email"
+            placeholder="Email"
+            className="mb-[16px] w-[300px]"
+            name={"email"} value={email}
+            onChange={(e: any) => setEmail(e.target.value)}
+          /> */}
+          {/* <Button text="Send me a magic link" className="w-[300px]" isPrimary onClick={async () => {
+            const user = await signIn('email', {
+              email: email,
+              redirect: false,
+              callbackUrl: '/',
+            })
+          }} /> */}
         </div>
-        <Input
-          type="email"
-          placeholder="Email"
-          className="mb-[16px] w-[300px]"
-          name={"email"} value={email}
-          onChange={(e: any) => setEmail(e.target.value)}
-        />
-        <Button text="Send me a magic link" className="w-[300px]" isPrimary onClick={handleSignIn} />
-      </div>
-    );
-  }, [sessionData, email, handleSignIn]);
+      )
+    } else {
+      return null
+    }
+  }
 
-  const renderMessage = ({ title, description }: { title: string, description: string }) => (
+  if (session?.status === 'loading') return (
     <div className="
       flex 
       w-full 
@@ -90,35 +108,28 @@ const LoginContainer = () => {
       ">
       <div className="w-[360px] flex flex-col items-center px-[24px] text-center">
         <img src="/logos/logo-icon-only-v2.svg" alt="Plannly" className="mb-[32px]" width={70} height={70} />
-        <h2 className="text-normal mb-[12px]">{title}</h2>
-        <p className="text-basic_grey_1 mb-[32px]">{description}</p>
+        <h2 className="text-normal mb-[12px]">Loading...</h2>
       </div>
     </div>
   )
 
-
-  const renderContent = () => {
-    if (sessionStatus === 'loading') {
-      return 'Loading...';
-    }
-    if (isEmployeeExist || error) {
-      return renderMessage(
-        {
-          title: 'Oops! Something went wrong',
-          description: 'Please contact your employer'
-        }
-      );
-    }
-    if (emailSent) {
-      return renderMessage(
-        {
-          title: 'Check your email',
-          description: 'We have sent you a magic link to sign in'
-        }
-      );
-    }
-    return renderForm();
-  };
+  if (noEmployeeAccount) return (
+    <div className="
+      flex 
+      w-full 
+      h-full 
+      justify-center 
+      xl:custom-bg
+      bg-basic_white
+      items-center
+      ">
+      <div className="w-[360px] flex flex-col items-center px-[24px] text-center">
+        <img src="/logos/logo-icon-only-v2.svg" alt="Plannly" className="mb-[32px]" width={70} height={70} />
+        <h2 className="text-normal mb-[12px]">Oops! someting did not work.</h2>
+        <p className="text-basic_grey_1 mb-[32px]">Please contact support</p>
+      </div>
+    </div>
+  )
 
   return (
     <div className="
@@ -130,9 +141,10 @@ const LoginContainer = () => {
       bg-basic_white
       items-center
       ">
-      {renderContent()}
+      {/* {renderSignInError()} */}
+      {renderForm()}
     </div>
-  );
-};
+  )
+}
 
-export default LoginContainer;
+export default LoginContainer
